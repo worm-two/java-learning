b396033c105ccd21c3cf2f2250c57a0b5521c4a9
yuming <yuming@gmail.com>
2025-05-17 17:30:35 +0800

    [elasticsearch学习] 创建模块

---
+++ elasticsearch-learning/src/main/java/com/taoyuan/learning/elasticsearch/ElasticSearchApplicationService.java
@@ -0,0 +1,10 @@
+package com.taoyuan.learning.elasticsearch;
+
+/**
+ * @Author: yuming
+ * @CreateTime: 2025-05-17 17:28
+ * @Description: elasticsearch启动类
+ * @Version: 1.0
+ */
+public class ElasticSearchApplicationService {
+}
